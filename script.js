a640675173588b6fc3bf6c654c86375f482acf93
Nikita <nlevin6@uwo.ca>
2023-10-01 14:59:33 -0400

    properly functioning search results box

--- script.js
+++ script.js
@@ -1,135 +1,6 @@
-function searchFunc() {
-    let countryQuery = document.getElementById("countryBar").value;
-    let currencyQuery = document.getElementById("currencyBar").value;
-
-    //check if both boxes are blank so that no alert pops up (idk it acts weird without this so i guess i gotta do this)
-    if (countryQuery.length === 0 && currencyQuery.length === 0) {
-        return false;
-    }
-
-    //checks for the country search box
-    if (countryQuery.length > 20) {
-        alert("more than 20 characters!");
-        return false;
-    }
-
-    //checks for the currency search box
-    if (currencyQuery.length > 0 && !/^[A-Z]{3}$/.test(currencyQuery)) {
-        alert("Only 3 upper case letters are allowed!");
-        return false;
-    }
-
-    //it doesn't wanna fetch from a json file for me for some reason so I'm storing all the data here in a const variable
-    const data = [
-        {
-            "name": "Andorra",
-            "currency": "EUR"
-        },
-        {
-            "name": "Austria",
-            "currency": "EUR"
-        },
-        {
-            "name": "Australia",
-            "currency": "AUD"
-        },
-        {
-            "name": "Belgium",
-            "currency": "EUR"
-        },
-        {
-            "name": "Bulgaria",
-            "currency": "BGN"
-        },
-        {
-            "name": "Canada",
-            "currency": "CAD"
-        },
-        {
-            "name": "Switzerland",
-            "currency": "CHF"
-        },
-        {
-            "name": "Ivory Coast",
-            "currency": "XOF"
-        },
-        {
-            "name": "China",
-            "currency": "CNY"
-        },
-        {
-            "name": "Czechia",
-            "currency": "CZK"
-        },
-        {
-            "name": "Germany",
-            "currency": "EUR"
-        },
-        {
-            "name": "Denmark",
-            "currency": "DKK"
-        },
-        {
-            "name": "Greece",
-            "currency": "EUR"
-        },
-        {
-            "name": "Israel",
-            "currency": "ILS"
-        },
-        {
-            "name": "India",
-            "currency": "INR"
-        },
-        {
-            "name": "Italy",
-            "currency": "EUR"
-        },
-        {
-            "name": "Japan",
-            "currency": "JPY"
-        },
-        {
-            "name": "South Korea",
-            "currency": "KRW"
-        },
-        {
-            "name": "Lebanon",
-            "currency": "LBP"
-        },
-        {
-            "name": "Slovakia",
-            "currency": "EUR"
-        }
-
-    ]
-
-    let matches = [];
-
-    //filters for the country search bar
-    if (countryQuery.length > 0) {
-        matches = data.filter(item => item.name.toLowerCase().includes(countryQuery.toLowerCase()));
-    }
-
-    //filters for the currency search bar
-    if (currencyQuery.length > 0) {
-        matches = data.filter(item => item.currency.toUpperCase().includes(currencyQuery.toUpperCase()));
-    }
-
-    //code for showing the up to 5 matches in the alert box
-    let msg = "";
-    for (let i = 0; i < Math.min(5, matches.length); i++) {
-        msg += `Name: ${matches[i].name}, Currency: ${matches[i].currency}\n`;
-    }
-    if (msg.length > 0) {
-        alert(msg);
-    } else {
-        alert("No matches");
-    }
-}
-
 //dynamic search results
 function searchUpdate() {
+    console.log("test");
     let countryQuery = document.getElementById("countryBar").value;
     let currencyQuery = document.getElementById("currencyBar").value;
     let searchResultsList = document.querySelector(".search-results-list");
@@ -217,14 +88,6 @@ function searchUpdate() {
         }
 
     ]
-    //check if both boxes are blank so that no alert pops up
-    if (countryQuery.length === 0 && currencyQuery.length === 0) {
-        while (searchResultsList.firstChild) {
-            searchResultsList.removeChild(searchResultsList.firstChild);
-        }
-        return false;
-
-    }
 
     //checks for the country search box
     if (countryQuery.length > 20) {
@@ -243,6 +106,12 @@ function searchUpdate() {
         searchResultsList.removeChild(searchResultsList.firstChild);
     }
 
+    // //check if both boxes are blank so that no results show up
+    // if (countryQuery.length === 0 && currencyQuery.length === 0) {
+    //     searchResultsList.style.display = "none";
+    //     return false;
+    // }
+
     //filters for the country search bar
     if (countryQuery.length > 0) {
         matches = data.filter(item => item.name.toLowerCase().includes(countryQuery.toLowerCase()));
@@ -260,4 +129,18 @@ function searchUpdate() {
         li.appendChild(text);
         searchResultsList.appendChild(li);
     }
+    // Display the search results if there are matches
+    if (matches.length > 0) {
+        searchResultsList.style.display = "block";
+    } else {
+        // Hide the search results if there are no matches
+        searchResultsList.style.display = "none";
+    }
+    console.log("Matches: ", matches);
 }
+
+const countryInput = document.getElementById("countryBar");
+const currencyInput = document.getElementById("currencyBar");
+
+countryInput.addEventListener("input", searchUpdate);
+currencyInput.addEventListener("input", searchUpdate);
